829757db7a49f58ec918205ba5f8b37f95b4dac1
WILLIAM <tech@MacBook-Pro.local>
2023-05-13 16:46:51 -0300

    ajustes media query

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  theme:{
+    screens:{
+      'xl':{'max':'1200px'},
+      'lg':{'max':'991px'},
+      'md':{'max':'767px'},
+      'sm':{'max':'550px'},
+      'xsm':{'max':'375px'},
+    }
+  }
 })
